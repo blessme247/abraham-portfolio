a6403b89621fa82b70f249ac5da282da1e04ad86
Abraham Solabi <solabiabraham@gmail.com>
2023-05-20 05:37:10 +0100

    Added new project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 gsap.from(".socials", {duration: 8, y: 80, ease: "bounce"});
 gsap.from(".image", {duration: 10, scale: 0.4, opacity: 0, ease: "bounce"});
-gsap.from(".project-card", {duration: 6, scale: 0.1, ease: "bounce"});
+// gsap.from(".project-card", {duration: 6, scale: 0.1, ease: "bounce"});
 
 const navbar = document.querySelector('.navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
